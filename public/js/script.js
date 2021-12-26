e7e5a122b355d45b3a2c923c21c9a943ea566df1
AntimonyIQ <antimonyiq@gmail.com>
2021-12-26 19:07:44 +0100

    added stuidd

---
+++ public/js/script.js
@@ -0,0 +1,47 @@
+function slider1() {
+    var n1 = document.getElementById("n1");
+    var n2 = document.getElementById("n2");
+    var n3 = document.getElementById("n3");
+    var n4 = document.getElementById("n4");
+
+    n1.style.display = "none";
+    n2.style.display = "block";
+    n3.style.display = "none";
+    n4.style.display = "none";
+}
+
+function slider2() {
+    var n1 = document.getElementById("n1");
+    var n2 = document.getElementById("n2");
+    var n3 = document.getElementById("n3");
+    var n4 = document.getElementById("n4");
+
+    n1.style.display = "none";
+    n2.style.display = "none";
+    n3.style.display = "block";
+    n4.style.display = "none";
+}
+
+function slider3() {
+    var n1 = document.getElementById("n1");
+    var n2 = document.getElementById("n2");
+    var n3 = document.getElementById("n3");
+    var n4 = document.getElementById("n4");
+
+    n1.style.display = "none";
+    n2.style.display = "none";
+    n3.style.display = "none";
+    n4.style.display = "block";
+}
+
+function slider4() {
+    var n1 = document.getElementById("n1");
+    var n2 = document.getElementById("n2");
+    var n3 = document.getElementById("n3");
+    var n4 = document.getElementById("n4");
+
+    n1.style.display = "block";
+    n2.style.display = "none";
+    n3.style.display = "none";
+    n4.style.display = "none";
+}
